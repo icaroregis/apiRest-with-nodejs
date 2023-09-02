43942e4d3c2885156c0989be939a024fb7301d4c
icaroalmeida-somma <icaro.almeida@somma-it.com>
2023-09-02 19:57:05 -0300

    Automação com eslint + instalação da lib dotenv

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import db from './config/dbConnect.js';
 import routes from './routes/index.js';
 
+//teste
+
 db.on('error', console.error.bind(console, 'Erro de conexão MongoDB:'));
 db.once('open', () => {
   console.log('Conexão com MongoDB Atlas estabelecida com sucesso!');
